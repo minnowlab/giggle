5b4517ab3d9523f46c5a948b471735123383325f
kai209209 <361561797@qq.com>
2013-10-28 22:25:19 +0800

    finish CRUD method

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,2 +1,31 @@
 class Admin::ProductsController < ApplicationController
+ def index
+
+ end
+
+ def show
+
+ end
+
+ def new
+ 
+ end
+
+ def create
+
+ end
+ 
+ def edit
+  
+ end
+
+ def update
+
+ end
+
+ def destroy
+ 
+ end
+
+
 end
