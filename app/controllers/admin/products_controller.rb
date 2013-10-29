73bd47b8ca00b42769eb96d364679cfa29e0b0bf
kai209209 <361561797@qq.com>
2013-10-29 14:38:42 +0800

    Finish CRUD

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,31 +1,42 @@
 class Admin::ProductsController < ApplicationController
  def index
-
+    @products=Product.all
  end
 
  def show
-
+    @product=Product.find(params[:id])
  end
 
  def new
- 
+    @product=Product.new
  end
 
  def create
-
+    @product=Product.new(product_params)
+    if @product.save
+    else
+      render 'new'
+    end
  end
- 
+
  def edit
-  
+    @product=Product.find(params[:id])
  end
 
  def update
-
+    if @product.update_attributes(product_params)
+    else
+      render 'edit'
+    end
  end
 
  def destroy
- 
+    Product.find(params[:id]).destroy
  end
 
+ private
+   def product_params
+     params.require(:product).permit(:name,:description,:detail,:price)
+   end
 
 end
