9a5e404bd9dfffe3ec847b34315c8b7dfab8f989
ray <frayay@gmail.com>
2013-11-08 12:23:22 +0800

    add admin products spec text

--- spec/controllers/admin/products_controller_spec.rb
+++ spec/controllers/admin/products_controller_spec.rb
@@ -1,5 +1,59 @@
 require 'spec_helper'
 
 describe Admin::ProductsController do
+  describe 'GET#index' do
+    it 'renders the :index view' do
+      get :index
+      expect(response).to render_template :index
+    end
+  end
 
+  describe'GET#new'do
+    it "assigns a new Product to @product" do
+      get :new
+      expect(assigns(:product)).to be_a_new(Product) 
+    end
+
+    it "renders the :new template" do 
+      get :new
+      expect(response).to render_template :new
+    end
+  end
+
+  describe'GET#edit'do
+    it "assigns the requested product to @product" do
+      product = create(:product)
+      get :edit, id: product
+      expect(assigns(:product)).to eq product
+    end
+
+    it "renders the :edit template" do
+      product = create(:product)
+      get :edit, id: product
+      expect(response).to render_template :edit
+    end 
+  end
+
+  describe"POST#create"do
+    before :each do
+      @product = [
+        attributes_for(:product),
+        attributes_for(:product),
+        attributes_for(:product)
+      ]
+    end
+
+    context "with valid attributes" do
+      it "saves the new product in the database" do
+        expect{
+          post :create, product: attributes_for(:product, products_attributes: @product)
+        }.to change(Product, :count).by(1)
+      end
+
+      it "redirects to products#index" do
+        post :create, product: attributes_for(:product, products_attributes: @product)
+        expect(response).to redirect_to admin_products_path
+      end 
+    end
+  end
 end
---
+++ spec/factories/product.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :product do
+    name "MyString"
+    description "MyString"
+    details "MyString"
+    price 15.2
+  end
+end
