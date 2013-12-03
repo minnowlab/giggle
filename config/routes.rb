b0337aecd632d41191a1b2535b9bb334fc0de917
ray <frayay@gmail.com>
2013-12-03 12:13:15 +0800

    add create product_picture form and action

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Giggle::Application.routes.draw do
-  get "evaluates/index"
-  get "evaluates/show"
-  get "evaluates/new"
-  get "evaluates/edit"
   get '/sign-in', to: 'sessions#new', as: :user_sign_in
   delete '/sign-out', to: 'sessions#destroy', as: :user_sign_out
   resources :sessions, only: :create
 
   namespace :admin do
     root 'products#index'
-    resources :products
+    resources :products do
+      member do
+        post :create_product_picture
+      end
+    end
     resources :messages 
 #    do
 #      collection do
