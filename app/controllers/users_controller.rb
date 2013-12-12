a295f20c6ca46f939e0b27663757304452a9dda7
zky <361561797@qq.com>
2013-12-12 11:00:40 +0800

    add user layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  layout "user"
   before_action :find_user, only: [:show, :edit]
   def index
   	@user = User.page(params[:page])
@@ -12,8 +13,23 @@ class UsersController < ApplicationController
   	
   end
 
+  def update
+    @user = User.find(users_params)
+    
+  end
+
+  def create
+    
+  end
+
+
   private
     def find_user
       @user = User.find(params[:id])
     end
+
+    def users_params
+      params.require(:user).permit(:email, :name, :password, :password_confirmation)
+    end
+
 end
