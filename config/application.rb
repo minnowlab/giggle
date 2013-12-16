f599d2c239b23630134a7178a3328269b5dcf465
ray <frayay@gmail.com>
2013-12-17 00:53:37 +0800

    setting the i18n default locale is zh-CN

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,6 @@ module Giggle
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default = "zh-CN"
+    config.i18n.default_locale = "zh-CN"
   end
 end
