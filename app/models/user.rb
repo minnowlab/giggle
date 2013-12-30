5f48b1df9579f0aad2022fb461d38b1985873748
kai209209 <361561797@qq.com>
2013-12-30 17:59:29 +0800

    add roles in admin/users/index page

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ActiveRecord::Base
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   ROLES = %w[admin manager user]
+  ROLES_cn = %w[超级管理员 管理员 用户]
   attr_accessor :skip_password
   
   validates :email, presence:   true,
@@ -70,6 +71,12 @@ class User < ActiveRecord::Base
     ROLES.reject do |r|
       ((roles_mask.to_i || 0) & 2**ROLES.index(r)).zero?
     end
+  end
+
+  def roles_cn
+    ROLES_cn.reject do |r|
+      ((roles_mask.to_i || 0) & 2**ROLES_cn.index(r)).zero?
+    end.join(',')
   end
 
   def is?(role)
