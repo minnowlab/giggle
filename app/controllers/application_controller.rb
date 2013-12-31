0fb4f0406761daa951f9eb0c301698628994a6ba
ray <frayay@gmail.com>
2013-12-31 18:00:35 +0800

    catch AccessDenied exception and redirect_to root_url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
-  
+
   before_action do
     resource = controller_name.singularize.to_sym
     method = "#{resource}_params"
     params[resource] &&= send(method) if respond_to?(method, true)
+  end
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, danger: exception.message
   end
 end
