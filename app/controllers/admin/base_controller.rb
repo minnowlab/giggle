941192d859795deef8d492740f3ca34df808408a
kai209209 <361561797@qq.com>
2013-12-31 17:15:01 +0800

    modify authorize for user

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,5 +1,12 @@
 class Admin::BaseController < ApplicationController
+  before_action :authorized_manage
   before_action :signed_in_user
   layout "admin"
   load_and_authorize_resource
+
+
+  private
+    def authorized_manage
+      redirect_to root_path unless current_user.is?(:manager) or current_user.is?(:admin)
+    end
 end
