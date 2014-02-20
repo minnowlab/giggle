029090404a837e8af5237efd78ba01dc074d67ce
ray <frayay@gmail.com>
2014-02-20 17:01:40 +0800

    修改收藏和喜欢的样式，并且修复bug

--- app/helpers/likeships_helper.rb
+++ app/helpers/likeships_helper.rb
@@ -1,15 +1,15 @@
 module LikeshipsHelper
   def likeable_likes_tag like
-    @like = Kernel.const_get(like[:likeable_type]).find(like[:likeable_id])
+    @like = Object.const_get(like[:likeable_type]).where(id: like[:likeable_id]).first
     return fa_icon("heart") if current_user.blank?
-    class_name = "icon_mark_select"
+    class_name = "icon_like_select"
     link_title = "喜欢"
     link_path_method = "post"
     if current_user and current_user.liking?(like)
-      class_name = "icon_mark_selected"
+      class_name = "icon_like_selected"
       link_title = "取消喜欢"
       link_path_method = "delete"
     end
-    link_to fa_icon("heart"), likeship_path(like), class: class_name, "data-toggle"=>"tooltip", title: link_title, method: link_path_method, remote: true
+    link_to "#{fa_icon('heart')} #{@like.likeships.count}人喜欢".html_safe, likeship_path(like), class: class_name, "data-toggle"=>"tooltip", title: link_title, method: link_path_method, remote: true
   end
 end
