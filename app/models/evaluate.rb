616d90b05ddb7a81777b84bf98de9cff0b9d41ab
zky <361561797@qq.com>
2014-09-10 15:09:55 +0800

    优化evaluate search代码

--- app/models/evaluate.rb
+++ app/models/evaluate.rb
@@ -17,12 +17,10 @@ class Evaluate < ActiveRecord::Base
   end
 
   def self.search this_params
-    products_ids = Product.where("name like ?", "%#{this_params[:product]}%").map(&:id) if this_params[:product].present?
-    users_ids = User.where("name LIKE ?", "%#{this_params[:user]}%").map(&:id) if this_params[:user].present?
     evaluate = Evaluate.all
     evaluate = evaluate.where("title LIKE ?", "%#{this_params[:title]}%") if this_params[:title].present?
-    evaluate = evaluate.where(product_id: products_ids) if this_params[:product].present?
-    evaluate = evaluate.where(user_id: users_ids) if this_params[:user].present?
+    evaluate = evaluate.joins(:product).where("products.name LIKE ?", "%#{this_params[:product]}%") if this_params[:product].present?
+    evaluate = evaluate.joins(:user).where("users.name LIKE ?", "%#{this_params[:user]}%") if this_params[:user].present?
     evaluate.paginate(page: this_params[:page])
   end
 
