2d0fb63b5cb045c1f45449a038a6d06e32bba721
ray <frayay@gmail.com>
2014-09-12 17:56:09 +0800

    添加user的valid测试

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,5 +1,40 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = users(:christelle)
+  end
 
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "should not save user without email" do
+    @user.email = nil
+    assert_not @user.save
+  end
+
+  test "should not save user without name" do
+    @user.name = nil
+    assert_not @user.save
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+
+  test "should not save user without password" do
+    @user.password = nil
+    assert_not @user.save
+  end
+
+  test "should not save user password and password_confirmation not the same" do
+    @user.password = "123456789"
+    @user.password_confirmation = "123456"
+    assert_not @user.save
+  end
 end
