94ed9c11a9de94e4c794072fff176edb676daaf6
ray <frayay@gmail.com>
2014-09-16 20:39:08 +0800

    添加 product 的search 测试

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -9,4 +9,25 @@ class ProductTest < ActiveSupport::TestCase
     @product.name = nil
     assert_not @product.save
   end
+
+  test "should not save product without product category" do
+    @product.product_category = nil
+    assert_not @product.save
+  end
+
+  test "should find user by name" do
+    assert_equal Product.search(email: @product.name), [@product]
+  end
+
+  test "should find user by min price" do
+    assert_equal Product.search(min_price: 3), [@product]
+  end
+
+  test "should find user by max price" do
+    assert_equal Product.search(max_price: 12), [@product]
+  end
+
+  test "should find user by product category id" do
+    assert_equal Product.search(product_category_id: @product.product_category_id), [@product]
+  end
 end
