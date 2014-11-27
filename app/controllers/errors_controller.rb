77e535aad0875526a3c1f66299ff39abf1105b8e
zky <361561797@qq.com>
2014-11-27 16:11:41 +0800

    访问错误页面时断开数据库查询

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,4 +1,6 @@
 class ErrorsController < ApplicationController
+  after_action :destroy_database_connect
+  
   def show
     render status_code.to_s, status: status_code
   end
@@ -7,5 +9,9 @@ class ErrorsController < ApplicationController
 
   def status_code
     params[:code] || 500
+  end
+
+  def destroy_database_connect    
+    ActiveRecord::Base.clear_active_connections!
   end
 end
